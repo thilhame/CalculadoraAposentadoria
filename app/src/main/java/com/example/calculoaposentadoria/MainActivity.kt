10c1b772b11a3842a33b5a9874e39a24966e4e21
thilhame <tlincon@uol.com.br>
2022-08-23 19:26:34 -0300

    refatorando o programa

--- app/src/main/java/com/example/calculoaposentadoria/MainActivity.kt
+++ app/src/main/java/com/example/calculoaposentadoria/MainActivity.kt
@@ -3,14 +3,15 @@ package com.example.calculoaposentadoria
 import android.app.Activity
 import android.os.Bundle
 import android.widget.*
-import org.w3c.dom.Text
 
 
-class MainActivity: Activity() {
+class MainActivity : Activity() {
 
-private lateinit var txt_idade:EditText
-private lateinit var spn_sexo:Spinner
-private lateinit var btn_calcular:Button
+    private lateinit var txt_idade: EditText
+    private lateinit var spn_sexo: Spinner
+    private lateinit var btn_calcular: Button
+    private lateinit var btn_limpar: Button
+    private lateinit var btn_sair: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,21 +19,17 @@ private lateinit var btn_calcular:Button
         //definindo o arquivo de layout
         setContentView(R.layout.activity_main)
 
-         spn_sexo = findViewById(R.id.spn_sexo)
-         txt_idade = findViewById(R.id.txt_idade)
-         btn_calcular = findViewById(R.id.btn_calcular)
-        val txt_resultado = findViewById<TextView>(R.id.txt_resultado)
+        spn_sexo = findViewById(R.id.spn_sexo)
+        txt_idade = findViewById(R.id.txt_idade)
+        btn_calcular = findViewById(R.id.btn_calcular)
+        btn_limpar = findViewById(R.id.btn_limpar)
+        btn_sair = findViewById(R.id.btn_sair)
+        var txt_resultado = findViewById<TextView>(R.id.txt_resultado)
 
-        spn_sexo.adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,
-        listOf(" ","masculino", "feminino"))
+        val arraySpinner =listOf(" ", "masculino", "feminino")
+        spn_sexo.adapter = ArrayAdapter<String>(this,R.layout.spinner,arraySpinner)
 
-
-
-
-
-        btn_calcular.setOnClickListener{
-
-
+        btn_calcular.setOnClickListener {
 
             val sexo = spn_sexo.selectedItem as String
             val idade = txt_idade.text.toString()
@@ -40,46 +37,36 @@ private lateinit var btn_calcular:Button
             var masculino = 70
             var feminino = 65
 
+            try {
 
+                if (sexo == "masculino" && idade.isNotEmpty()) {
+                    resultado = (masculino - idade.toInt())
+                    txt_resultado.text = ("Faltam $resultado anos para você se aposentar. ")
 
+                } else if (sexo == "feminino" && idade.isNotEmpty()) {
+                    resultado = (feminino - idade.toInt())
+                    txt_resultado.text = "Faltam $resultado anos para você se aposentar. "
 
+                }else{
+                    Toast.makeText(getApplicationContext(), "Preencha todos os campos", Toast.LENGTH_LONG).show();
+                }
+            } catch (e: Throwable) {
 
-            if ( sexo == "masculino" && idade.isNotEmpty()){
-                resultado = (masculino - idade.toInt())
-                txt_resultado.text = "Faltam $resultado anos para você se aposentar . "
-
-
-            }else{
-                Toast.makeText(getApplicationContext(),"Preencha todos os campos",Toast.LENGTH_SHORT).show();
-                validarDados()
             }
 
-
-            if(sexo == "feminino" && idade.isNotEmpty()){
-                resultado = (feminino - idade.toInt())
-                txt_resultado.text = "Faltam $resultado anos para você se aposentar . "
-
-            }else{
-                Toast.makeText(getApplicationContext(),"Preencha todos os campos",Toast.LENGTH_SHORT).show();
-                validarDados()
-            }
+        }
+        btn_limpar.setOnClickListener() {
+            txt_idade.text.clear()
+            txt_resultado.setText("")
 
 
+        }
+        btn_sair.setOnClickListener() {
+            finish()
 
         }
 
     }
 
-    private fun validarDados() : Boolean {
-
-            var error = false
-            if (txt_idade.text.isEmpty()) {
-                txt_idade.error = "obrigatorio preencher campos!"
-                error = true
-            }
-            return error
-
-        }
-
-    }
+   }
 
